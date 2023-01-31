092fff4662f73805bef014f11ac73b226d741f0a
Sean Molenaar <sean@seanmolenaar.eu>
2023-01-31 20:15:54 +0100

    Add swift build info

--- Package.swift
+++ Package.swift
@@ -1,9 +1,22 @@
-//
-//  Package.swift
-//  Fosdem
-//
-//  Created by Sean Molenaar on 31/01/2023.
-//  Copyright © 2023 Sean Molenaar. All rights reserved.
-//
+// swift-tools-version:5.3
+import PackageDescription
 
-import Foundation
+let package = Package(
+    name: "Fosdem",
+    platforms: [
+        .iOS("16.0")
+    ],
+    products: [
+    ],
+    dependencies: [
+        .package(url: "https://github.com/yahoojapan/SwiftyXMLParser.git", from: "5.0.0"),
+    ],
+    targets: [
+        .target(name: "Fosdem", path: "Fosdem"),
+        .testTarget(
+            name: "FosdemTests",
+            dependencies: ["Fosdem"],
+            path: "FosdemTests"
+        ),
+    ]
+)
